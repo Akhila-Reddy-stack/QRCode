6306a8a2f7ceb45be08ea4229af303e3804f064d
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-02-02 00:04:46 +0800

    Add files via upload

--- qrgenerator.js
+++ qrgenerator.js
@@ -1,1 +1,30 @@
+const QRCode = require('qrcode');
+const Excel = require('xlsx');
+
+const ws = Excel.readFile("ProductDetails.xlsx").Sheets["Sheet1"];
+const data = Excel.utils.sheet_to_json(ws);
+
+
+const myFunc = async () => {
+    return new Promise((res, rej) => {
+        data.map(ele => {
+            console.log(ele,"eleee")
+            QRCode.toFile(
+                ele.ProductName + ".png",
+                JSON.stringify([ele]),
+                () => {
+                    res(true);
+                    console.log('callback')
+                }
+            )
+        })
+
+    });
+}
+
+
+(async () => {
+    await myFunc()
+})()
+console.log('almost done');
 
